dongx1x/confidential-cloud-native-primitives

service: add ccnp webhook to add ccnp server sock automatically
Signed-off-by: Xiaocheng Dong <xiaocheng.dong@intel.com>

service/ccnp-webhook/main.go
@@ -1,0 +1,123 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"os"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	//+kubebuilder:scaffold:imports
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	//+kubebuilder:scaffold:scheme
+}
+
+func main() {
+	var metricsAddr string
+	var probeAddr string
+	var secureMetrics bool
+	var certDir string
+	var keyName string
+	var certName string
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&secureMetrics, "metrics-secure", false,
+		"If set the metrics endpoint is served securely")
+	flag.StringVar(&certDir, "cert-dir", "", "Folder where key-name and cert-name are located.")
+	flag.StringVar(&keyName, "key-name", "", "Filename for .key file.")
+	flag.StringVar(&certName, "cert-name", "", "Filename for .cert file.")
+
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	tlsOpts := []func(*tls.Config){}
+
+	webhookServer := webhook.NewServer(webhook.Options{
+		TLSOpts: tlsOpts,
+	})
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress:   metricsAddr,
+			SecureServing: secureMetrics,
+			TLSOpts:       tlsOpts,
+		},
+		WebhookServer:          webhookServer,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         false,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	server := webhook.NewServer(webhook.Options{
+		Port:     9443,
+		CertDir:  certDir,
+		KeyName:  keyName,
+		CertName: certName,
+	})
+
+	server.Register("/mutate", &webhook.Admission{Handler: &podCcnpAdmission{
+		Client: mgr.GetClient(),
+	}})
+
+	mgr.Add(server)
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}

service/ccnp-webhook/pod_webhook.go
@@ -1,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+const VolumeAnnotation = "ccnp.cc-api/require"
+const CcnpServerSock = "/run/ccnp/uds/ccnp-server.sock"
+
+type podCcnpAdmission struct {
+	Client         client.Client
+	decoder        *admission.Decoder
+	Annotation     bool
+	StorageClasses []string
+}
+
+func (a *podCcnpAdmission) Handle(ctx context.Context, req admission.Request) admission.Response {
+	pod := &corev1.Pod{}
+	if err := a.decoder.Decode(req, pod); err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	// check for the existence of a pod annotation if enabled
+	if a.Annotation {
+		value, ok := pod.Annotations[VolumeAnnotation]
+		if !ok {
+			return admission.Allowed("Got no pod annotation.")
+		}
+
+		parsed, err := strconv.ParseBool(value)
+		if err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+
+		if !parsed {
+			return admission.Allowed("Pod annotation says false.")
+		}
+	}
+
+	pathType := corev1.HostPathSocket
+	sockName := "ccnp-server-sock"
+	pod.Spec.Volumes = append(pod.Spec.Volumes, corev1.Volume{
+		Name: sockName,
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{
+				Path: CcnpServerSock,
+				Type: &pathType,
+			},
+		},
+	})
+
+	for c := range pod.Spec.Containers {
+		container := &pod.Spec.Containers[c]
+		container.VolumeMounts = append(container.VolumeMounts, corev1.VolumeMount{
+			Name:      sockName,
+			ReadOnly:  false,
+			MountPath: CcnpServerSock,
+		})
+	}
+
+	marshaledPod, err := json.Marshal(pod)
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+
+	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledPod)
+}
+
+func (a *podCcnpAdmission) InjectDecoder(d *admission.Decoder) error {
+	a.decoder = d
+	return nil
+}

cc-api/container-integrity-measurement-agent

service/ccnp-webhook/main.go
@@ -1,0 +1,125 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"os"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	//+kubebuilder:scaffold:imports
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	//+kubebuilder:scaffold:scheme
+}
+
+func main() {
+	var metricsAddr string
+	var probeAddr string
+	var secureMetrics bool
+	var certDir string
+	var keyName string
+	var certName string
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&secureMetrics, "metrics-secure", false,
+		"If set the metrics endpoint is served securely")
+	flag.StringVar(&certDir, "cert-dir", "", "Folder where key-name and cert-name are located.")
+	flag.StringVar(&keyName, "key-name", "", "Filename for .key file.")
+	flag.StringVar(&certName, "cert-name", "", "Filename for .cert file.")
+
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	tlsOpts := []func(*tls.Config){}
+
+	webhookServer := webhook.NewServer(webhook.Options{
+		TLSOpts: tlsOpts,
+	})
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress:   metricsAddr,
+			SecureServing: secureMetrics,
+			TLSOpts:       tlsOpts,
+		},
+		WebhookServer:          webhookServer,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         false,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	server := webhook.NewServer(webhook.Options{
+		Port:     9443,
+		CertDir:  certDir,
+		KeyName:  keyName,
+		CertName: certName,
+	})
+
+	server.Register("/mutate", &webhook.Admission{Handler: &podCcnpWebhook{
+		Client:  mgr.GetClient(),
+		decoder: admission.NewDecoder(mgr.GetScheme()),
+	}})
+
+	mgr.Add(server)
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}

service/ccnp-webhook/pod_webhook.go
@@ -1,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+const CcnpAnnotation = "ccnp.cc-api/require"
+const CcnpServerSockDir = "/run/ccnp/uds/"
+
+type podCcnpWebhook struct {
+	Client     client.Client
+	decoder    *admission.Decoder
+	Annotation bool
+}
+
+func NewPodCcnpWebhook() *podCcnpWebhook {
+	return &podCcnpWebhook{}
+}
+
+func (a *podCcnpWebhook) Handle(ctx context.Context, req admission.Request) admission.Response {
+	pod := &corev1.Pod{}
+	if err := a.decoder.Decode(req, pod); err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	// check for the existence of a pod annotation if enabled
+	if a.Annotation {
+		value, ok := pod.Annotations[CcnpAnnotation]
+		if !ok {
+			return admission.Allowed("Got no pod annotation.")
+		}
+
+		parsed, err := strconv.ParseBool(value)
+		if err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+
+		if !parsed {
+			return admission.Allowed("Pod annotation says false.")
+		}
+	}
+
+	pathType := corev1.HostPathDirectory
+	sockName := "ccnp-server-sock"
+	pod.Spec.Volumes = append(pod.Spec.Volumes, corev1.Volume{
+		Name: sockName,
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{
+				Path: CcnpServerSockDir,
+				Type: &pathType,
+			},
+		},
+	})
+
+	for c := range pod.Spec.Containers {
+		container := &pod.Spec.Containers[c]
+		container.VolumeMounts = append(container.VolumeMounts, corev1.VolumeMount{
+			Name:      sockName,
+			ReadOnly:  false,
+			MountPath: CcnpServerSockDir,
+		})
+	}
+
+	marshaledPod, err := json.Marshal(pod)
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+
+	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledPod)
+}
+
+func (a *podCcnpWebhook) InjectDecoder(d *admission.Decoder) error {
+	a.decoder = d
+	return nil
+}